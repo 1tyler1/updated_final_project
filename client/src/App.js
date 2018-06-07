bd8e82f22f1bf085af50dc8c3cd7b79e4b02fc7e
1tyler1 <mya12ga@yahoo.com>
2018-06-07 18:39:55 -0400

    udpating user page

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 
-
-import HomePage from './components/HomePage'
-import LogInPage from './components/LogInPage'
-import PhotoPage from './components/Photos/PhotoPage'
-import ShowUserPage from './components/Users/ShowUserPage'
-import NewPhotoFormPage from './components/Photos/NewPhotoFormPage'
-import Weight from './components/Weight'
-
-
-
+import HomePage from './components/HomePage';
+import LoginPage from './components/LoginPage'
+import ShowUsers from './components/Users/ShowUsers.js';
+import ArtPage from './components/ArtPage';
 
 class App extends Component {
   render() {
     return (
-     
       <div>
         <Router>
-          
-            <Switch>
-
-            {/* user routes */}
-              <Route exact path="/" component={HomePage} />
-             {/* delete and show in the show route */} 
-              <Route exact path="/user" component={LogInPage} />
-              <Route exact path="/user/:userId" component={ShowUserPage} />
-              {/* photos routes */}
-              <Route exact path='/user/:userId/photos' component={PhotoPage} />
-              <Route exact path='/user/:userId/photos/:photoId' component={NewPhotoFormPage} />
-              <Route exact path='/user/:userId/weight' component={Weight} />
-             
-              {/* delete and show in the show route */}
-             
-             
-           
-
-            </Switch>
-
+        <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/user" component={LoginPage} />
+        <Route exact path="/user/:userId" component={ShowUsers} />
+        <Route exact path="/user/:userId/art" component={ArtPage} />
+        </Switch>
         </Router>
-
-      
-      <footer class="footer">Tyler Lauren Designs</footer>      
-     
       </div>
-    )
+    );
   }
 }
 
